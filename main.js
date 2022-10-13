b82ef65a86e3c533a334d4019b73670ff4f74e40
threebarber <dvtdropshot@gmail.com>
2022-10-13 09:36:40 -0400

    functionality and display of data capabilities added

--- main.js
+++ main.js
@@ -6,10 +6,46 @@ function createUrl(location) {
 }
 
 async function retrieveJson(url) {
-  console.log(`fetching URL: ${url}`);
-  const respRaw = await fetch(url);
+  try {
+    console.log(`fetching URL: ${url}`);
+    const respRaw = await fetch(url);
+  
+    console.log("waiting for data....");
+    const responseData = await respRaw.json();
+    console.log(responseData);
+    return responseData;
 
-  console.log("waiting for data....");
-  const responseData = await respRaw.json();
-  return responseData;
+  } catch (error) {
+    console.log(`Error occurred: ${error}`);
+    alert(`An error occured: ${error}`);
+  }
+  
+}
+
+
+
+function displayWeather(jsonData){
+
+    var locationElement = document.querySelector("#locationText");
+    var tempElement = document.querySelector("#tempText");
+
+    var weatherElement = document.querySelector("#weatherDescriptionText");
+    var minMaxElement = document.querySelector("#minMaxText");
+
+
+    locationElement.textContent = jsonData.name;
+    tempElement.textContent = `Temp: ${jsonData.main.temp}`;
+    weatherElement.textContent = jsonData.weather[0].main;
+    minMaxElement.textContent = `Min: ${jsonData.main.temp_min} Max: ${jsonData.main.temp_max}`;
+
+
+
+}
+
+
+
+async function run(){
+    let url = createUrl("towson");
+    let jsonData = await retrieveJson(url);
+    displayWeather(jsonData);
 }
