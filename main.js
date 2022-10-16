6bf9cfb6b619bee7ad9eebe37d83f29cad280c53
threebarber <dvtdropshot@gmail.com>
2022-10-16 12:30:11 -0400

    error messages and input validation requiring a value to be entered

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.querySelector("#searchButton").addEventListener("click", function () {
+  if(!document.querySelector("#searchInput").validity.valueMissing){
     run();
-  });
-
+  }else{
+    alert("Enter a location, zip code or search term");
+  }
+});
 
 function createUrl(location) {
   return (
@@ -14,44 +17,46 @@ async function retrieveJson(url) {
   try {
     console.log(`fetching URL: ${url}`);
     const respRaw = await fetch(url);
-  
+
     console.log("waiting for data....");
     const responseData = await respRaw.json();
     console.log(responseData);
     return responseData;
-
   } catch (error) {
     console.log(`Error occurred: ${error}`);
     alert(`An error occured: ${error}`);
   }
-  
 }
 
+function displayWeather(jsonData) {
+  var locationElement = document.querySelector("#locationText");
+  var tempElement = document.querySelector("#tempText");
 
+  var weatherElement = document.querySelector("#weatherDescriptionText");
+  var minMaxElement = document.querySelector("#minMaxText");
 
-function displayWeather(jsonData){
+  let statusCode = jsonData.cod;
 
-    var locationElement = document.querySelector("#locationText");
-    var tempElement = document.querySelector("#tempText");
+  console.log(`RESPONSE CODE: ${statusCode}`);
 
-    var weatherElement = document.querySelector("#weatherDescriptionText");
-    var minMaxElement = document.querySelector("#minMaxText");
-
-
+  if (statusCode == "200") {
     locationElement.textContent = jsonData.name;
     tempElement.textContent = `Temp: ${jsonData.main.temp}`;
     weatherElement.textContent = jsonData.weather[0].main;
     minMaxElement.textContent = `Min: ${jsonData.main.temp_min} Max: ${jsonData.main.temp_max}`;
+  }else{
+    tempElement.textContent = `Error Occurred: ${statusCode}`;
 
+    locationElement.textContent = "";
+    weatherElement.textContent = "";
+    minMaxElement.textContent = "";
 
-
+  }
 }
 
-
-
-async function run(){
-    let userSearchInput = document.querySelector("#searchInput").value;
-    let url = createUrl(userSearchInput);
-    let jsonData = await retrieveJson(url);
-    displayWeather(jsonData);
+async function run() {
+  let userSearchInput = document.querySelector("#searchInput").value;
+  let url = createUrl(userSearchInput);
+  let jsonData = await retrieveJson(url);
+  displayWeather(jsonData);
 }
